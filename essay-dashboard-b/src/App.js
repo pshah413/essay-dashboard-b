f7e4ace784019e2bcd5258124d3f0779fe0cb887
Denise Li <deniseli11@163.com>
2021-11-07 01:12:28 -0500

    create NavBar and Google Login

--- essay-dashboard-b/src/App.js
+++ essay-dashboard-b/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login'
+import Home from './pages';
+// import About from './pages/about';
+// import Services from './pages/services';
+// import Contact from './pages/contact';
+// import SignUp from './pages/signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </Router>
   );
 }
 
